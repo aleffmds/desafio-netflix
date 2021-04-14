0b9039fb45653e907b50fb0951da83e178d1bc0c
Cyber <aleffmartinsds@gmail.com>
2021-04-14 00:20:49 -0300

    Desafio concluidogit add *

---
+++ js/owl/botoes.js
@@ -0,0 +1,7 @@
+function abrirVideo(){
+    window.open("https://www.youtube.com/watch?v=d6kBeJjTGnY");
+}
+
+function abrirInfo(){
+    window.open("https://pt.wikipedia.org/wiki/Hunter_%C3%97_Hunter");  
+}
